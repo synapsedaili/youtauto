53f0f06e59e00e5a2a3bf5da3e2d9b3507fe3a6f
synapsedaili <Avrlg969@outlook.com.tr>
2026-01-07 21:42:12 +0300

    Delete src/tts.py

--- src/tts.py
+++
@@ -1,8 +0,0 @@
-# src/tts.py
-import asyncio
-import edge_tts
-
-async def generate_voice_with_edge_tts(text: str, output_path: str):
-    """Edge TTS ile ses üretir."""
-    communicate = edge_tts.Communicate(text, "en-US-GuyNeural")
-    await communicate.save(output_path)
